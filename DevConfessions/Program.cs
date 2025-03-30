bec8beb5d2fbdea4bc199511ab94fcd074f36534
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2025-03-30 03:20:03 -0300

    Update Program.cs

--- DevConfessions/Program.cs
+++ DevConfessions/Program.cs
@@ -5,9 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<JsonConfessionService>();
 
-// Antes de builder.Build():
-var dataDir = Path.Combine(Directory.GetCurrentDirectory(), "data");
-if (!Directory.Exists(dataDir)) Directory.CreateDirectory(dataDir);
+var dataPath = Path.Combine(app.Environment.WebRootPath, "data");
+if (!Directory.Exists(dataPath)) 
+{
+    Directory.CreateDirectory(dataPath);
+}
 
 var app = builder.Build();
 
